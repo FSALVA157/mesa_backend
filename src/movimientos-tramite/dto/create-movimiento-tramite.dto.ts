69e4dff387bc879ec2e853b6d263eb7d0d685d1f
pedrodiaz0487 <pedrodiaz0487@gmail.com>
2023-03-06 19:58:32 -0300

    verificacion de validacion en create movimientos - falta validar enteros correctamente

--- src/movimientos-tramite/dto/create-movimiento-tramite.dto.ts
+++ src/movimientos-tramite/dto/create-movimiento-tramite.dto.ts
@@ -1,29 +1,34 @@
-import { IsDateString, IsInt, IsNotEmpty, IsString, MaxLength, MinLength, IsBoolean, IsEmpty, IsOptional, Length, Min, Max, Matches } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsDateString, IsInt, IsNotEmpty, IsString, MaxLength, MinLength, IsBoolean, IsEmpty, IsOptional, Length, Min, Max, Matches, IsNumber, IsDivisibleBy } from 'class-validator';
+
+
 
 export class CreateMovimientoTramiteDto {
     
     //@IsInt({message: "El campo Número de movimiento debe ser entero"})
     num_movimiento_tramite: number;
 
-    @IsInt({message: "El campo número de tramite debe ser entero"})
+    @IsInt({message: "Número de tramite debe ser un número entero"})
     tramite_numero: number;
    
-    @IsInt({message: "El campo Sector origen debe ser entero"})
+    @IsInt({message: "Sector origen debe ser un número entero"})
     sector_origen_id: number;
     
     //@IsDateString()
     fecha_ingreso: Date
     
-    @Matches(RegExp('^[0-9]+$'),{message:'Fojas ingreso debe ser un número entero'})
+    //@Matches(/^[0-9]*$/,{message:'Fojas ingreso debe ser un número entero'})
+    
     @Max(10000,{message: 'Fojas ingreso no debe ser mayor a 10000(diez mil)'})
     @Min(0,{message: 'Fojas ingreso debe ser 0(cero) o mayor'})    
+    @IsInt({message: "Fojas ingreso debe ser un número entero"})
     fojas_ingreso: number;
            
     @Length(1,500,{message:'La Descripción ingreso debe tener entre $constraint1 y $constraint2 caracteres'})
     descripcion_ingreso: string;
 
     @IsOptional()
-    @IsInt({message: "El campo Sector destino debe ser entero"})
+    @IsInt({message: "Sector destino debe ser un número entero"})
     sector_destino_id: number;
     
     @IsOptional()
@@ -31,8 +36,8 @@ export class CreateMovimientoTramiteDto {
     fecha_salida: Date;
     
     @IsOptional()
-    @IsInt({message: "El campo Fojas salida debe ser entero"})
-    @Min(0,{message: 'Las Fojas salida debe ser 0(cero) o mayor'})
+    @IsInt({message: "Fojas salida debe ser un número entero"})
+    @Min(0,{message: 'Fojas salida debe ser 0(cero) o mayor'})
     @Max(10000,{message: 'La Fojas salida no debe ser mayor a 10000(cero)'})
     fojas_salida: number;   
 
@@ -51,9 +56,9 @@ export class CreateMovimientoTramiteDto {
     @IsEmpty({message: "No debe enviar el campo impreso"})
     impreso: boolean;
     
-    @IsInt({message: "El campo Sector debe ser entero"})
+    @IsInt({message: "Sector debe ser un número entero"})
     sector_id: number;    
    
-    @IsInt({message: "El campo Usuario debe ser entero"})
+    @IsInt({message: "Usuario debe ser un número entero"})
     usuario_id: number;
 }
